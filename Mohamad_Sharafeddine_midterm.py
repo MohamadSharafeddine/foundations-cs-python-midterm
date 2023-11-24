379a14b28f8ddcff1f1064ddca1b18bc3b4f218a
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2023-11-24 23:59:16 +0200

    display menu function created

--- Mohamad_Sharafeddine_midterm.py
+++ Mohamad_Sharafeddine_midterm.py
@@ -1,1 +1,4 @@
-# test
+def displayMenu():
+    print("1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Sort All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit")
+
+displayMenu()
