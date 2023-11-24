a6cad92b32f0d8ca6ba4bd82910e889255b40fcb
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2023-11-25 00:06:22 +0200

    created main functions

--- Mohamad_Sharafeddine_midterm.py
+++ Mohamad_Sharafeddine_midterm.py
@@ -1,4 +1,26 @@
 def displayMenu():
     print("1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Sort All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit")
 
-displayMenu()
+def openTab():
+    pass
+
+def closeTab():
+    pass
+
+def switchTab():
+    pass
+
+def displayAllTabs():
+    pass
+
+def openNestedTab():
+    pass
+
+def sortAllTabs():
+    pass
+
+def saveTabs():
+    pass
+
+def importTabs():
+    pass
