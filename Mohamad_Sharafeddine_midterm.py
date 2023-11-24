1eb409282ef85441671472d8faffa7d12b924c8a
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2023-11-25 01:30:17 +0200

    basic openTab function done

--- Mohamad_Sharafeddine_midterm.py
+++ Mohamad_Sharafeddine_midterm.py
@@ -1,8 +1,15 @@
+tabs = []
+
 def displayMenu():
     print("1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Sort All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit")
 
 def openTab():
-    pass
+    title = input("Enter tab's title: ")
+    url = input("Enter tab's URL: ")
+    new_tab = {title:url}
+    tabs.append(new_tab)
+    print(f"{title} opened")
+    print(tabs)
 
 def closeTab():
     pass
