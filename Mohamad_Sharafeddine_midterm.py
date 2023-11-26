0ffb89feb6177397896b6718b2fffb6c2a1379fe
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2023-11-26 09:41:12 +0200

    documented displayMenu

--- Mohamad_Sharafeddine_midterm.py
+++ Mohamad_Sharafeddine_midterm.py
@@ -13,6 +13,7 @@ def insertionSort(list1): #O(n^2)
             current -= 1
         border += 1
 
+# Function displays menu
 def displayMenu():
     print("\n1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tab\n6. Sort All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit\n")
 
